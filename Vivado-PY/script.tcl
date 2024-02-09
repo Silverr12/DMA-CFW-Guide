358c7e419c64f2597b0af35194603951704e795e
Rakesh Monkee <jlharrison05@gmail.com>
2024-02-09 11:07:16 +1000

    Add files via upload

--- Vivado-PY/script.tcl
+++ Vivado-PY/script.tcl
@@ -1,6 +1,6 @@
 source vivado_generate_project.tcl -notrace
-
-cd C:/Users/jlhar/Desktop/Vivado-Test/pcileech-fpga-4.13/PCIeSquirrel/pcileech_squirrel/pcileech_squirrel.srcs/sources_1/ip/pcie_7x_0
+#change  dir to yours
+cd C:/Users/user/Desktop/Vivado-Test/pcileech-fpga-4.13/PCIeSquirrel/pcileech_squirrel/pcileech_squirrel.srcs/sources_1/ip/pcie_7x_0
 
 generate_target {all} [get_files pcie_7x_0.xci]
 
